d143c319d497b25b941123a84e820dca66c674d6
Juraj Haluska <juraj.haluska@innovatrics.com>
2026-04-01 12:14:26 +0000

    Version 4.71.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 4.71.0 - 2026-04-01
+### Changed
+- Update DOT SDK to 9.2.2.
+
 ## 4.70.0 - 2026-03-20
 ### Changed
 - Update DOT SDK to 9.2.1.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
             version = release(version = 36)
         }
         versionCode = 1
-        versionName = "4.70.0"
+        versionName = "4.71.0"
     }
 
     buildTypes {
